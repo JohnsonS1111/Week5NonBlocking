71f9ee20a1949a8353808ef5a70902d19cdff2d5
Johnson Shogbaike <G00389668@atu.ie>
2023-10-23 15:50:00 +0100

    Made new NonBlocking application

---
+++ src/main/java/ie/atu/nonblocking/NonBlockingApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.nonblocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonBlockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonBlockingApplication.class, args);
+	}
+
+}
